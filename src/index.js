f2dff4ad35051eda0f28674b7ef3b734d56bdf42
rddpgn <rddpgn@gmail.com>
2020-01-15 15:40:25 +0300

    Закончена работа над функционалом header (адаптивность, скрытые меню)

--- src/index.js
+++ src/index.js
@@ -1,16 +1,27 @@
-let mobileMenuButton = document.getElementById('mobile-menu-button');
-let mobileMenu = document.getElementById('mobile-menu');
-let mobileMenuBc = document.getElementById('mobile-menu-bc');
-let mobileMenuButtonHide = document.getElementById('mobile-menu-button-hide');
+{
+    let mobileMenuButton = document.getElementById('mobile-menu-button');
+    let mobileMenu = document.getElementById('mobile-menu');
+    let mobileMenuBc = document.getElementById('mobile-menu-bc');
+    let mobileMenuButtonHide = document.getElementById('mobile-menu-button-hide');
 
-window.onload = () => {
-    mobileMenuButton.onclick = () => {
-        mobileMenu.classList.toggle('mobile-menu_hidden');
+    window.onload = () => {
+        mobileMenuButton.onclick = () => {
+            mobileMenu.classList.toggle('mobile-menu_hidden');
+        };
+        mobileMenuButtonHide.onclick = () => {
+            mobileMenu.classList.toggle('mobile-menu_hidden');
+        };
+        mobileMenuBc.onclick = () => {
+            mobileMenu.classList.toggle('mobile-menu_hidden');
+        } 
     };
-    mobileMenuButtonHide.onclick = () => {
-        mobileMenu.classList.toggle('mobile-menu_hidden');
-    };
-    mobileMenuBc.onclick = () => {
-        mobileMenu.classList.toggle('mobile-menu_hidden');
-    } 
 };
+
+let header = document.getElementById('header');
+window.addEventListener('scroll', () => {
+    if (window.scrollY > 32) {
+        header.classList.add('header_float');
+    } else {
+        header.classList.remove('header_float');
+    }
+});
