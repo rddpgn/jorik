7e493c8917b663bedb82bcbcad34c41ee912e6b0
rddpgn <rddpgn@gmail.com>
2020-01-15 21:25:08 +0300

    Закончена работа над карточками товаров

--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
-{
+
     let mobileMenuButton = document.getElementById('mobile-menu-button');
     let mobileMenu = document.getElementById('mobile-menu');
     let mobileMenuBc = document.getElementById('mobile-menu-bc');
     let mobileMenuButtonHide = document.getElementById('mobile-menu-button-hide');
+    let quads = document.getElementsByClassName('quad');
 
     window.onload = () => {
         mobileMenuButton.onclick = () => {
@@ -13,15 +14,33 @@
         };
         mobileMenuBc.onclick = () => {
             mobileMenu.classList.toggle('mobile-menu_hidden');
-        } 
+        };
+        makeQuads(quads);
     };
-};
 
-let header = document.getElementById('header');
-window.addEventListener('scroll', () => {
-    if (window.scrollY > 32) {
-        header.classList.add('header_float');
-    } else {
-        header.classList.remove('header_float');
+
+
+    let header = document.getElementById('header');
+    window.addEventListener('scroll', () => {
+        if (window.scrollY > 32) {
+            header.classList.add('header_float');
+        } else {
+            header.classList.remove('header_float');
+        }
+    });
+    
+    function resizedw(){
+        makeQuads(quads);
     }
-});
+    
+    let doit;
+    window.onresize = function(){
+      clearTimeout(doit);
+      doit = setTimeout(resizedw, 100);
+    };
+    
+    function makeQuads(elems) {
+        for(let elem of elems) {
+            elem.style.height = `${elem.offsetWidth}px`;
+        }
+    };
